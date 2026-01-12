b6530627754e2bb8c196c8abf508e7c85fda3846
krisna <krisna.w2010@gmail.com>
2026-01-12 19:35:25 +0800

    wip landing page

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import {db} from "@/db/db";
+import {magicLink} from "better-auth/plugins";
 
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
+  plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, token, url }, ctx) => {
+        console.log(email, token, url)
+        console.log(ctx)
+      }
+    })
+  ]
 });
