efb2a24234c396a5f553de9a001463df6f358f0a
krisna <krisna.w2010@gmail.com>
2026-01-12 22:24:46 +0800

    wip auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Footer from "@/components/landing/footer";
+import Header from "@/components/landing/header";
 
 export default function Home() {
   return (
     <div className="h-screen">
+
+      <Header />
 
       <main>
         Main
