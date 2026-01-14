48cd1fd9c134603c43c48921dfc3aba94ce15818
krisna <krisna.w2010@gmail.com>
2026-01-14 17:30:54 +0800

    wip event crud

--- app/(home)/event/[id]/page.tsx
+++ app/(home)/event/[id]/page.tsx
@@ -84,7 +84,7 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
 
               <div>
                 <div className="px-4 sm:px-0 border-b border-gray-200">
-                  <h3 className="text-base/7 font-semibold ">About Event</h3>
+                  <h3 className="text-base/7 font-semibold text-muted-foreground">About Event</h3>
                 </div>
                 <div className="mt-4">
                   <p>
@@ -103,11 +103,14 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
               <div>
 
                 <div className="px-4 sm:px-0 border-b border-gray-200">
-                  <h3 className="text-base/7 font-semibold ">Location</h3>
+                  <h3 className="text-base/7 font-semibold text-muted-foreground">Location</h3>
                 </div>
 
-                <div>
-                  sdf
+                <div className="mt-4">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.3628429589126!2d115.26138180000001!3d-8.656998999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd241007ff18975%3A0x90cb3b1c40257b08!2sXavi%20Croissanterie!5e0!3m2!1sen!2sid!4v1768382812451!5m2!1sen!2sid"
+                    width="600" height="450" style={{ border: 0 }} allowFullScreen={false} loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
               </div>
 
