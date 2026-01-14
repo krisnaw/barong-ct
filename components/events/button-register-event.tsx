bec954ed8e5fec78c5ac1b8912764148bafe9723
krisna <krisna.w2010@gmail.com>
2026-01-14 11:03:53 +0800

    wip event feature

---
+++ components/events/button-register-event.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import {Button} from "@/components/ui/button";
+
+export function ButtonRegisterEvent({eventId} : {eventId: number}) {
+  return (
+    <Button>
+      Join
+    </Button>
+  )
+}
--- components/events/list-item-event.tsx
+++ components/events/list-item-event.tsx
@@ -1,9 +1,13 @@
 import {EventType} from "@/db/schema";
 import {MapPin} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {ButtonRegisterEvent} from "@/components/events/button-register-event";
 
 export function ListItemEvent({event} : {event: EventType}) {
   return (
     <div className="flex outline rounded-xl p-4">
+
       <div className="mr-4 shrink-0">
         <img
           alt=""
@@ -11,15 +15,27 @@ export function ListItemEvent({event} : {event: EventType}) {
           className="inline-block size-24 rounded-md outline -outline-offset-1 outline-white/10"
         />
       </div>
-      <div>
-        <p className="text-gray-500 text-lg">{new Date().toLocaleDateString()}</p>
-        <h4 className="text-lg font-bold ">
-          {event.name}
-        </h4>
-        <p className="mt-1 inline-flex items-center text-gray-500 gap-2">
-          <MapPin size={18} /> Capital Place
-        </p>
+
+      <div className="w-full flex flex-wrap items-center justify-between sm:flex-nowrap">
+        <div>
+          <p className="text-gray-500 text-lg">{new Date().toLocaleDateString()}</p>
+          <h4 className="text-lg font-bold ">
+            {event.name}
+          </h4>
+          <p className="mt-1 inline-flex items-center text-gray-500 gap-2">
+            <MapPin size={18} /> Capital Place
+          </p>
+        </div>
+        <div className="shrink-0 flex gap-2">
+          <Button variant="outline" asChild>
+            <Link href={`/event/${event.id}`}>
+              View
+            </Link>
+          </Button>
+          <ButtonRegisterEvent eventId={event.id} />
+        </div>
       </div>
+
     </div>
   )
 }
