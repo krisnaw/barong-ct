c2d1a5db4f4f097c53ad92762d29add1e075fa7e
krisna <krisna.w2010@gmail.com>
2026-01-15 14:58:53 +0800

    test query

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -1,62 +1,75 @@
 import {getEvents} from "@/db/query/event-query";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {EventDate} from "@/components/events/event-date";
 
 export default async function EventsPage() {
 
-  const events = await getEvents()
+  const events = await getEvents();
 
+  if (events.length === 0) {
+    return (
+      <Button asChild>
+        <Link href="/dashboard/events/create">
+          Create Event
+        </Link>
+      </Button>
+    )
+  }
 
   return (
     <div>
       event page
-      {/*<div className="flex justify-between items-center mb-4">*/}
-      {/*  <h1 className="text-2xl font-bold">Events</h1>*/}
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">Events</h1>
 
-      {/*<Button asChild>*/}
-      {/*  <Link href="/dashboard/events/create">*/}
-      {/*    Create Event*/}
-      {/*  </Link>*/}
-      {/*</Button>*/}
+      <Button asChild>
+        <Link href="/dashboard/events/create">
+          Create Event
+        </Link>
+      </Button>
 
-      {/*</div>*/}
-      {/*<Table>*/}
-      {/*  <TableHeader>*/}
-      {/*    <TableRow>*/}
-      {/*      <TableHead>Name</TableHead>*/}
-      {/*      <TableHead>Date time</TableHead>*/}
-      {/*      <TableHead>Actions</TableHead>*/}
-      {/*    </TableRow>*/}
-      {/*  </TableHeader>*/}
-      {/*  <TableBody>*/}
-      {/*    {events.map((event) => (*/}
-      {/*      <TableRow key={event.id}>*/}
-      {/*        <TableCell className="font-medium">*/}
-      {/*          {event.name}*/}
-      {/*        </TableCell>*/}
-      {/*        <TableCell className="flex flex-col">*/}
-      {/*          <div className="font-medium ">*/}
-      {/*            <EventDate eventDate={event.startDate} type="date" />*/}
-      {/*          </div>*/}
-      {/*          <div className="mt-1 text-muted-foreground">*/}
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Date time</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {events.map((event) => (
+            <TableRow key={event.id}>
+              <TableCell className="font-medium">
+                {event.name}
+              </TableCell>
+              <TableCell className="flex flex-col">
+                <div className="font-medium ">
+                  <EventDate eventDate={event.startDate} type="date" />
+                </div>
+                <div className="mt-1 text-muted-foreground">
 
-      {/*            <EventDate eventDate={event.startDate} type="time" />*/}
-      {/*          </div>*/}
-      {/*        </TableCell>*/}
-      {/*        <TableCell>*/}
-      {/*          <Link href={`/dashboard/events/${event.id}`}>*/}
-      {/*            <Button variant="outline" size="sm" className="mr-2">*/}
-      {/*              View*/}
-      {/*            </Button>*/}
-      {/*          </Link>*/}
-      {/*          <Link href={`/dashboard/events/${event.id}/edit`}>*/}
-      {/*            <Button variant="outline" size="sm">*/}
-      {/*              Edit*/}
-      {/*            </Button>*/}
-      {/*          </Link>*/}
-      {/*        </TableCell>*/}
-      {/*      </TableRow>*/}
-      {/*    ))}*/}
-      {/*  </TableBody>*/}
-      {/*</Table>*/}
+                  <EventDate eventDate={event.startDate} type="time" />
+                </div>
+              </TableCell>
+              <TableCell>
+                <Link href={`/dashboard/events/${event.id}`}>
+                  <Button variant="outline" size="sm" className="mr-2">
+                    View
+                  </Button>
+                </Link>
+                <Link href={`/dashboard/events/${event.id}/edit`}>
+                  <Button variant="outline" size="sm">
+                    Edit
+                  </Button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
