0b6face2ebb5cee4f74c0216bfa41e2ce8629646
krisna <krisna.w2010@gmail.com>
2026-01-15 14:49:54 +0800

    test query

--- proxy.ts
+++ proxy.ts
@@ -18,5 +18,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/*", "/profile"], // Specify the routes the middleware applies to
+  matcher: ["/dashboard", "/profile"], // Specify the routes the middleware applies to
 };
