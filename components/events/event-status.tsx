c7ee0ae4bb091f7e2ecaaa6681259fc838361449
krisna <krisna.w2010@gmail.com>
2026-01-16 12:21:26 +0800

    use Next image

--- components/events/event-status.tsx
+++ components/events/event-status.tsx
@@ -3,13 +3,22 @@ import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import {ButtonJoinEvent} from "@/components/events/button-join-event";
 import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 export async function EventStatus({eventId}: {eventId: number}) {
   const session = await auth.api.getSession({
     headers: await headers() // you need to pass the headers object.
   })
 
-  if (!session) return null;
+  if (!session) {
+    return (
+      <Button className="w-full" type="submit">
+        <Link href="/auth/signup">
+          Please create account or sign in to join
+        </Link>
+      </Button>
+    )
+  }
 
   const event = await checkParticipantByEvent(eventId, session.user.id)
 
