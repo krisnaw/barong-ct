9b5da9cc842ad9ad2fe563ff90ae0e2261a2071a
krisna <krisna.w2010@gmail.com>
2026-02-09 14:25:35 +0800

    wip payment

--- app/(home)/(manage)/event/[id]/page.tsx
+++ app/(home)/(manage)/event/[id]/page.tsx
@@ -5,11 +5,13 @@ import {getOngoingOrder} from "@/db/query/event-order.query";
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import {EventStatus} from "@/components/events/event-status";
-import {CalendarDays, MapPin} from "lucide-react";
+import {CalendarDays, MapPin, TicketCheck} from "lucide-react";
 import {EventDate} from "@/components/events/event-date";
 import {EventCard} from "@/components/events/event-card";
 import {getPaymentByOrder} from "@/db/query/event-payment.query";
 import {EventPaymentCard} from "@/components/events/event-payment-card";
+import {getParticipantByEventUser} from "@/db/query/participant-query";
+import {Item, ItemContent, ItemDescription, ItemFooter, ItemMedia, ItemTitle} from "@/components/ui/item";
 
 export default async function Page({params,}: { params: Promise<{ id: number }> }) {
 
@@ -36,6 +38,8 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
     payment = await getPaymentByOrder(order.id)
   }
 
+  const participant =  await getParticipantByEventUser(id, userId)
+
   return (
     <div>
 
@@ -50,6 +54,45 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
         {payment && (
           <EventPaymentCard payment={payment} />
         )}
+
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <TicketCheck/>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Registrations</ItemTitle>
+          </ItemContent>
+
+          {participant && (
+            <ItemContent className="flex-none text-center py-1">
+              <ItemDescription>
+                <span className="uppercase inline-flex items-center rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
+                    {participant?.status}
+                </span>
+              </ItemDescription>
+            </ItemContent>
+          )}
+
+        </Item>
+
+        <Item variant="outline">
+
+          <ItemMedia variant="icon">
+            <MapPin/>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Locations</ItemTitle>
+          </ItemContent>
+
+          <ItemFooter>
+            <div className="w-full overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.3628429589126!2d115.26138180000001!3d-8.656998999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd241007ff18975%3A0x90cb3b1c40257b08!2sXavi%20Croissanterie!5e0!3m2!1sen!2sid!4v1768382812451!5m2!1sen!2sid"
+                width="470" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </ItemFooter>
+        </Item>
       </div>
 
 
@@ -148,12 +191,7 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
                   </div>
 
                   <div className="mt-4">
-                    <div className="w-full aspect-[16/9] rounded-lg overflow-hidden">
-                      <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.3628429589126!2d115.26138180000001!3d-8.656998999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd241007ff18975%3A0x90cb3b1c40257b08!2sXavi%20Croissanterie!5e0!3m2!1sen!2sid!4v1768382812451!5m2!1sen!2sid"
-                        width="600" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
+
                   </div>
                 </div>
 
