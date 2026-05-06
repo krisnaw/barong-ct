cb6c7e2ba77def034f14f3dcdb71012b696bedab
krisna <krisna.w2010@gmail.com>
2026-05-06 19:34:11 +0800

    fix image

--- app/(home)/(manage)/event/[id]/page.tsx
+++ app/(home)/(manage)/event/[id]/page.tsx
@@ -48,120 +48,119 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
   return (
     <div>
 
-      <div className="mx-auto max-w-xl px-4 md:px-6 lg:px-8 mt-8">
+      <div className="mx-auto max-w-xl px-4 md:px-6 lg:px-8 mt-8 space-y-6">
 
-        <EventCard event={event} withFooter={false} participant={participant}/>
+        <Card className="pt-0">
+          <CardHeader className="bg-muted/50 border-b p-4 items-center">
+            <CardTitle className="font-bold leading-7">
+              Registrations
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-2 gap-2">
 
-        <div className="mt-6 space-y-6">
+              <Item className="bg-green-50 col-span-2 inset-ring inset-ring-green-600/20">
+                <ItemContent>
+                  <ItemTitle
+                    className="uppercase cn-card-title cn-font-heading text-xl tabular-nums  text-green-700 text-center">
+                    Registration Confirmed
+                  </ItemTitle>
+                </ItemContent>
+              </Item>
 
-          <Card className="pt-0">
-            <CardHeader className="bg-muted/50 border-b p-4 items-center">
-              <CardTitle className="font-bold leading-7">
-                Registrations
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="grid grid-cols-2 gap-2">
 
-                <Item className="bg-green-50 col-span-2 inset-ring inset-ring-green-600/20">
+              {participant && (
+                <Item className="bg-muted">
                   <ItemContent>
-                    <ItemTitle className="uppercase cn-card-title cn-font-heading text-xl tabular-nums  text-green-700 text-center">
-                      Registration Confirmed
+                    <ItemDescription>Bib Number</ItemDescription>
+                    <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
+                      {participant.bibNumber && formatBibNumber(participant.bibNumber)}
                     </ItemTitle>
                   </ItemContent>
                 </Item>
+              )}
 
 
-                {participant && (
-                  <Item className="bg-muted">
-                    <ItemContent>
-                      <ItemDescription>Bib Number</ItemDescription>
-                      <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
-                        {participant.bibNumber && formatBibNumber(participant.bibNumber)}
-                      </ItemTitle>
-                    </ItemContent>
-                  </Item>
-                )}
+              {participant && (
+                <Item className="bg-muted">
+                  <ItemContent>
+                    <ItemDescription>Jersey Size</ItemDescription>
+                    <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
+                      M
+                    </ItemTitle>
+                  </ItemContent>
+                </Item>
+              )}
 
+              {payment && (
+                <Item className="bg-muted col-span-2">
+                  <ItemContent>
+                    <ItemDescription>
+                      Payment Success
+                    </ItemDescription>
+                    <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
+                      {payment.invoiceNumber}
+                    </ItemTitle>
+                  </ItemContent>
+                </Item>
+              )}
 
-                {participant && (
-                  <Item className="bg-muted">
-                    <ItemContent>
-                      <ItemDescription>Jersey Size</ItemDescription>
-                      <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
-                        M
-                      </ItemTitle>
-                    </ItemContent>
-                  </Item>
-                )}
+            </div>
+          </CardContent>
+        </Card>
 
-                {payment && (
-                  <Item className="bg-muted col-span-2">
-                    <ItemContent>
-                      <ItemDescription>
-                        Payment Success
-                      </ItemDescription>
-                      <ItemTitle className="cn-card-title cn-font-heading text-2xl tabular-nums text-primary">
-                        {payment.invoiceNumber}
-                      </ItemTitle>
-                    </ItemContent>
-                  </Item>
-                )}
+        <EventCard event={event} withFooter={false} participant={participant}/>
 
+        {group && (
+          <Card className="pt-0">
+            <CardHeader className="bg-muted/50 border-b p-4 items-center">
+              <CardTitle className="font-bold leading-7">
+                Group: {group.name}
+              </CardTitle>
+              <CardDescription className="text-muted-foreground">
+                {group.participants.length}/5 members
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="flex flex-col space-y-4">
+                {group.participants.map((name: string, index: number) => (
+                  <li className="inline-flex items-center gap-2" key={index}>
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png"/>
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <p className="font-bold">{name}</p>
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+            <CardFooter>
+              <InviteItem eventId={event.id} group={group}/>
+            </CardFooter>
+          </Card>
+        )}
+
+        {event.locationLink && (
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                Texas Tech University -Costa Rica
+              </CardTitle>
+              <CardDescription>
+                Avenida Escazú, Edificio AE205, San José Province, Escazu, 10201, Costa Rica
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="w-full overflow-hidden">
+                <iframe
+                  src={event.locationLink}
+                  width="470" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </CardContent>
           </Card>
+        )}
 
-          {group && (
-            <Card className="pt-0">
-              <CardHeader className="bg-muted/50 border-b p-4 items-center">
-                <CardTitle className="font-bold leading-7">
-                  Group: {group.name}
-                </CardTitle>
-                <CardDescription className="text-muted-foreground">
-                  {group.participants.length}/5 members
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <ul className="flex flex-col space-y-4">
-                  {group.participants.map((name: string, index: number) => (
-                    <li className="inline-flex items-center gap-2" key={index}>
-                      <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png"/>
-                        <AvatarFallback>CN</AvatarFallback>
-                      </Avatar>
-                      <p className="font-bold">{name}</p>
-                    </li>
-                  ))}
-                </ul>
-              </CardContent>
-              <CardFooter>
-                <InviteItem eventId={event.id} group={group}/>
-              </CardFooter>
-            </Card>
-          )}
-
-          {event.locationLink && (
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  Texas Tech University -Costa Rica
-                </CardTitle>
-                <CardDescription>
-                  Avenida Escazú, Edificio AE205, San José Province, Escazu, 10201, Costa Rica
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="w-full overflow-hidden">
-                  <iframe
-                    src={event.locationLink}
-                    width="470" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-              </CardContent>
-            </Card>
-          )}
-        </div>
       </div>
     </div>
   )
