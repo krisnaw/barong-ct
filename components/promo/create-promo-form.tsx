03ec5bafc80ebfdd29acdc6738138fb838fa5757
krisna <krisna.w2010@gmail.com>
2026-05-13 16:57:43 +0800

    wip discount type

--- components/promo/create-promo-form.tsx
+++ components/promo/create-promo-form.tsx
@@ -9,6 +9,9 @@ import {Button} from "@/components/ui/button";
 import {Spinner} from "@/components/ui/spinner";
 import {CustomDatePicker} from "@/components/ui/custom-date-picker";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
+import {Label} from "@/components/ui/label";
+import {Slider} from "@/components/ui/slider";
 import {createPromoAction} from "@/app/actions/profile/promo/promo.action";
 
 interface CreatePromoFormProps {
@@ -21,14 +24,16 @@ export function CreatePromoForm({ eventId, onSuccess }: CreatePromoFormProps) {
   const discountValueId = useId();
   const startsAtId = useId();
   const endsAtId = useId();
-  
   const [startsAt, setStartsAt] = useState<Date | null>(null);
   const [endsAt, setEndsAt] = useState<Date | null>(null);
+  const [discountType, setDiscountType] = useState("fixed")
+  const [discountPercentage, setDiscountPercentage] = useState(0)
 
   const [state, formAction, isPending] = useActionState<ActionResponse, FormData>(async (prevState: ActionResponse, formData: FormData) => {
     const payload = {
       eventId: eventId,
       promo: formData.get("promo") as string,
+      discountType: formData.get("discount_type") as string,
       discountValue: Number(formData.get("discountValue")),
       startsAt: startsAt,
       endsAt: endsAt,
@@ -60,23 +65,62 @@ export function CreatePromoForm({ eventId, onSuccess }: CreatePromoFormProps) {
           </Field>
 
           <Field>
-            <FieldLabel htmlFor={discountValueId}>
-              Discount Value (IDR)
-            </FieldLabel>
-            <Input
-              id={discountValueId}
-              type="number"
-              name="discountValue"
-              placeholder="10000"
-              min="0"
-              step="1"
-              required
-            />
-            <FieldDescription>
-              Enter fixed discount amount in IDR
-            </FieldDescription>
+            <FieldLabel htmlFor={promoId}>Discount Type</FieldLabel>
+            <RadioGroup defaultValue="fixed" name="discount_type" value={discountType} onValueChange={(value) => setDiscountType(value)}>
+              <div className="flex items-center gap-3">
+                <RadioGroupItem value="fixed" id="fixed" />
+                <Label htmlFor="fixed">Fixed</Label>
+              </div>
+              <div className="flex items-center gap-3">
+                <RadioGroupItem value="percentage" id="percentage" />
+                <Label htmlFor="percentage">Percentage</Label>
+              </div>
+            </RadioGroup>
           </Field>
 
+
+          {discountType === "fixed" ? (
+            <Field>
+              <FieldLabel htmlFor={discountValueId}>
+                Discount Value (IDR)
+              </FieldLabel>
+              <Input
+                id={discountValueId}
+                type="number"
+                name="discountValue"
+                placeholder="10000"
+                min="0"
+                step="1"
+                required
+              />
+              <FieldDescription>
+                Enter fixed discount amount in IDR
+              </FieldDescription>
+            </Field>
+          ) : (
+            <Field>
+              <FieldLabel htmlFor={discountValueId}>
+                Discount Percentage (IDR) {discountPercentage}
+              </FieldLabel>
+
+              <Slider
+                value={[discountPercentage]}
+                onValueChange={(val) => {
+                  // Array.at(0) or val[0] works once we ensure it's an array
+                  const newValue = Array.isArray(val) ? val[0] : val;
+                  setDiscountPercentage(newValue);
+                }}
+                max={100}
+                step={10}
+                className="w-full"
+              />
+
+              <FieldDescription>
+                Enter fixed discount amount in IDR
+              </FieldDescription>
+            </Field>
+          )}
+          
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <Field>
               <FieldLabel htmlFor={startsAtId}>Start Date & Time</FieldLabel>
