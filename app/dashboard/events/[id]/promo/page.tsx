d92f55def735cfb8724a7de71bc515e3fc82c55f
krisna <krisna.w2010@gmail.com>
2026-05-22 08:24:18 +0800

    wip edit category

--- app/dashboard/events/[id]/promo/page.tsx
+++ app/dashboard/events/[id]/promo/page.tsx
@@ -1,28 +1,6 @@
 import {CreatePromoForm} from "@/components/promo/create-promo-form";
-import {getPromoByEvent} from "@/db/query/event-promo.query";
-import {ListPromo} from "../../../../../components/promo/list-promo";
-import {getEventById} from "@/db/query/event-query";
-import {redirect} from "next/navigation";
 
 export default async function Page({params}: { params: Promise<{ id: number }> }) {
   const {id} = await params;
-
-  const promos = await getPromoByEvent(id)
-  const event = await getEventById(id)
-
-  if (!event) {
-    redirect('/dashboard')
-  }
-
-  return (
-    <div className="grid grid-cols-1 gap-4">
-      <CreatePromoForm eventId={id} price={event.price ?? 0} />
-
-      <div>
-        {promos && (
-          <ListPromo promos={promos} />
-        )}
-      </div>
-    </div>
-  )
+  return <CreatePromoForm eventId={id} />
 }
--- components/promo/create-promo-form.tsx
+++ components/promo/create-promo-form.tsx
@@ -13,6 +13,7 @@ import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
 import {Label} from "@/components/ui/label";
 import {Slider} from "@/components/ui/slider";
 import {createPromoAction} from "@/app/actions/profile/promo/promo.action";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 
 interface CreatePromoFormProps {
   eventId: number;
@@ -50,124 +51,128 @@ export function CreatePromoForm({ eventId, price }: CreatePromoFormProps) {
   }, initialState)
 
   return (
-    <div className="flex flex-col gap-6">
-      <form action={formAction}>
-        <FieldGroup>
+    <form action={formAction} className="max-w-3xl">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Promo</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <FieldGroup>
 
-          <Field>
-            <FieldLabel htmlFor={promoId}>Promo Code</FieldLabel>
-            <Input
-              id={promoId}
-              type="text"
-              name="promo"
-              placeholder="SAVE10"
-              required
-            />
-            <FieldDescription>
-              Enter the promo code that users will use
-            </FieldDescription>
-          </Field>
-
-          <Field>
-            <FieldLabel htmlFor={promoId}>Discount Type</FieldLabel>
-            <RadioGroup defaultValue="fixed" name="discount_type" value={discountType} onValueChange={(value) => setDiscountType(value)}>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem value="fixed" id="fixed" />
-                <Label htmlFor="fixed">Fixed</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem value="percentage" id="percentage" />
-                <Label htmlFor="percentage">Percentage</Label>
-              </div>
-            </RadioGroup>
-          </Field>
-
-
-          {discountType === "fixed" ? (
             <Field>
-              <FieldLabel htmlFor={discountValueId}>
-                Discount Value (IDR)
-              </FieldLabel>
+              <FieldLabel htmlFor={promoId}>Promo Code</FieldLabel>
               <Input
-                id={discountValueId}
-                type="number"
-                name="discountValue"
-                placeholder="10000"
-                min="0"
-                step="1"
+                id={promoId}
+                type="text"
+                name="promo"
+                placeholder="SAVE10"
                 required
               />
               <FieldDescription>
-                Enter fixed discount amount in IDR
-              </FieldDescription>
-            </Field>
-          ) : (
-            <Field>
-              <FieldLabel htmlFor={discountValueId}>
-                Discount Percentage (IDR) {discountPercentage}
-              </FieldLabel>
-
-              <Slider
-                value={[discountPercentage]}
-                onValueChange={(val) => {
-                  // Array.at(0) or val[0] works once we ensure it's an array
-                  const newValue = Array.isArray(val) ? val[0] : val;
-                  setDiscountPercentage(newValue);
-                }}
-                max={100}
-                step={10}
-                className="w-full"
-              />
-
-              <FieldDescription>
-                Enter fixed discount amount in IDR
-              </FieldDescription>
-            </Field>
-          )}
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <Field>
-              <FieldLabel htmlFor={startsAtId}>Start Date & Time</FieldLabel>
-              <CustomDatePicker name="startDate" value={startsAt || new Date()} />
-              <FieldDescription>
-                When the promo becomes active
+                Enter the promo code that users will use
               </FieldDescription>
             </Field>
 
             <Field>
-              <FieldLabel htmlFor={endsAtId}>End Date & Time</FieldLabel>
-              <CustomDatePicker name="endDate" value={endsAt || new Date()} />
+              <FieldLabel htmlFor={promoId}>Discount Type</FieldLabel>
+              <RadioGroup defaultValue="fixed" name="discount_type" value={discountType} onValueChange={(value) => setDiscountType(value)}>
+                <div className="flex items-center gap-3">
+                  <RadioGroupItem value="fixed" id="fixed" />
+                  <Label htmlFor="fixed">Fixed</Label>
+                </div>
+                <div className="flex items-center gap-3">
+                  <RadioGroupItem value="percentage" id="percentage" />
+                  <Label htmlFor="percentage">Percentage</Label>
+                </div>
+              </RadioGroup>
+            </Field>
+
+
+            {discountType === "fixed" ? (
+              <Field>
+                <FieldLabel htmlFor={discountValueId}>
+                  Discount Value (IDR)
+                </FieldLabel>
+                <Input
+                  id={discountValueId}
+                  type="number"
+                  name="discountValue"
+                  placeholder="10000"
+                  min="0"
+                  step="1"
+                  required
+                />
+                <FieldDescription>
+                  Enter fixed discount amount in IDR
+                </FieldDescription>
+              </Field>
+            ) : (
+              <Field>
+                <FieldLabel htmlFor={discountValueId}>
+                  Discount Percentage (IDR) {discountPercentage}
+                </FieldLabel>
+
+                <Slider
+                  value={[discountPercentage]}
+                  onValueChange={(val) => {
+                    // Array.at(0) or val[0] works once we ensure it's an array
+                    const newValue = Array.isArray(val) ? val[0] : val;
+                    setDiscountPercentage(newValue);
+                  }}
+                  max={100}
+                  step={10}
+                  className="w-full"
+                />
+
+                <FieldDescription>
+                  Enter fixed discount amount in IDR
+                </FieldDescription>
+              </Field>
+            )}
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <Field>
+                <FieldLabel htmlFor={startsAtId}>Start Date & Time</FieldLabel>
+                <CustomDatePicker name="startDate" value={startsAt || new Date()} />
+                <FieldDescription>
+                  When the promo becomes active
+                </FieldDescription>
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor={endsAtId}>End Date & Time</FieldLabel>
+                <CustomDatePicker name="endDate" value={endsAt || new Date()} />
+                <FieldDescription>
+                  When the promo expires (optional)
+                </FieldDescription>
+              </Field>
+            </div>
+
+            <Field>
+              <FieldLabel htmlFor="isActive">Status</FieldLabel>
+              <Select defaultValue="true" name="isActive">
+                <SelectTrigger className="w-45">
+                  <SelectValue placeholder="Select status" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="true">Active</SelectItem>
+                  <SelectItem value="false">Inactive</SelectItem>
+                </SelectContent>
+              </Select>
               <FieldDescription>
-                When the promo expires (optional)
+                Set whether the promo is currently active
               </FieldDescription>
             </Field>
-          </div>
+          </FieldGroup>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" disabled={isPending} size="lg">
+            {isPending ? <Spinner /> : null }
+            Save
+          </Button>
+        </CardFooter>
+      </Card>
 
-          <Field>
-            <FieldLabel htmlFor="isActive">Status</FieldLabel>
-            <Select defaultValue="true" name="isActive">
-              <SelectTrigger className="w-45">
-                <SelectValue placeholder="Select status" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="true">Active</SelectItem>
-                <SelectItem value="false">Inactive</SelectItem>
-              </SelectContent>
-            </Select>
-            <FieldDescription>
-              Set whether the promo is currently active
-            </FieldDescription>
-          </Field>
-
-          <Field>
-            <Button type="submit" disabled={isPending} size="lg">
-              {isPending ? <Spinner /> : null }
-              Create Promo
-            </Button>
-          </Field>
-
-        </FieldGroup>
-      </form>
-    </div>
+    </form>
   )
 }
