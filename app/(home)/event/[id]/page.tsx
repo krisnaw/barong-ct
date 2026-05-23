2f72e85b00bb7b9d6ea7eecb4a4ab976edc26fd3
krisna <krisna.w2010@gmail.com>
2026-05-23 17:57:03 +0800

    fix deleted route page

--- app/(home)/event/[id]/page.tsx
+++ app/(home)/event/[id]/page.tsx
@@ -4,7 +4,7 @@ import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import * as React from "react";
 import Image from "next/image";
-import {Card, CardAction, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import Link from "next/link";
 import {getOngoingOrder} from "@/db/query/event-order.query";
 import {buttonVariants} from "@/components/ui/button";
@@ -15,6 +15,9 @@ import {getPaymentByOrder} from "@/db/query/event-payment.query";
 import {getParticipantByEventUser} from "@/db/query/participant-query";
 import {formatBibNumber} from "@/utils/money-helper";
 import {EventDate} from "@/components/events/event-date";
+import {getGroupById} from "@/db/query/event-group.query";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {InviteItem} from "@/app/(home)/event/[id]/invite-item";
 
 export default async function Page({params,}: { params: Promise<{ id: number }> }) {
 
@@ -35,11 +38,13 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
   }
 
   const order = await getOngoingOrder(id, session.user.id);
-  let payment = undefined
-  let participant = undefined
+  let payment, participant, group = undefined
   if (order) {
-    payment = await getPaymentByOrder(order.id)
-    participant = await getParticipantByEventUser(id, session.user.id);
+    [payment, participant, group] = await Promise.all([
+      getPaymentByOrder(order.id),
+      getParticipantByEventUser(id, session.user.id),
+      getGroupById(order.groupId!)
+    ]);
   }
 
   return (
@@ -92,7 +97,6 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
 
             </CardContent>
           </Card>
-
 
           {order && order.status == ORDER_STATUS.COMPLETED && (
             <>
@@ -163,10 +167,91 @@ export default async function Page({params,}: { params: Promise<{ id: number }> 
                   </CardContent>
                 </Card>
               ) : null}
+
+              {group && (
+                <Card>
+                  <CardHeader>
+                    <CardTitle>
+                      Group Name: {group.name}
+                    </CardTitle>
+                    <CardAction>
+                      {group.participants.length}/5 members
+                    </CardAction>
+                  </CardHeader>
+                  <CardContent>
+                    <ul className="flex flex-col space-y-4">
+                      {group.participants.map((name: string, index: number) => (
+                        <li className="inline-flex items-center gap-2" key={index}>
+                          <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png"/>
+                            <AvatarFallback>CN</AvatarFallback>
+                          </Avatar>
+                          <p className="font-bold">{name}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardFooter>
+                    <InviteItem eventId={event.id} group={group}/>
+                  </CardFooter>
+                </Card>
+              )}
             </>
           )}
+
+          {event.locationLink && (
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  Texas Tech University -Costa Rica
+                </CardTitle>
+                <CardDescription>
+                  Avenida Escazú, Edificio AE205, San José Province, Escazu, 10201, Costa Rica
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="w-full overflow-hidden">
+                  <iframe
+                    src={event.locationLink}
+                    width="470" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </CardContent>
+            </Card>
+          )}
+
+          <Card className="p-0">
+            <CardContent className="px-0">
+              <div>
+                <div className="bg-white">
+                  <div className="mx-auto max-w-7xl">
+                    <div className="grid grid-cols-1 gap-px bg-gray-900/10 sm:grid-cols-2">
+                      {stats.map((stat) => (
+                        <div key={stat.name} className="bg-white px-4 py-6 sm:px-6 lg:px-8">
+                          <p className="flex items-baseline gap-x-2">
+                            <span className="text-4xl font-semibold tracking-tight text-gray-900">{stat.value}</span>
+                            {stat.unit ? <span className="text-sm text-gray-500">{stat.unit}</span> : null}
+                          </p>
+                          <p className="mt-2 text-sm/6 font-medium text-gray-500">{stat.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+
+
         </div>
       </div>
     </div>
   )
 }
+
+const stats = [
+  { name: 'Distance', value: '140', unit: 'km' },
+  { name: 'Elevation Gain', value: '2,370', unit: 'm' },
+  { name: 'Pit stops', value: '3' },
+  { name: 'Cutoff time', value: '5', unit: 'hours' },
+]
