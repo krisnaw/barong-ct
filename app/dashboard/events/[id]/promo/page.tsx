93f346ff200b038614ffe0dedee44610177eaa0a
krisna <krisna.w2010@gmail.com>
2026-05-23 17:24:47 +0800

    fix deleted route page

--- app/dashboard/events/[id]/promo/page.tsx
+++
@@ -1,6 +0,0 @@
-import {CreatePromoForm} from "@/components/promo/create-promo-form";
-
-export default async function Page({params}: { params: Promise<{ id: number }> }) {
-  const {id} = await params;
-  return <CreatePromoForm eventId={id} />
-}
