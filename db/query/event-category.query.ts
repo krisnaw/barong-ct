f1aa791e63edf346bff32dc0f5586d9b8d611cc8
krisna <krisna.w2010@gmail.com>
2026-06-12 11:40:16 +0800

    fix data shorting

--- db/query/event-category.query.ts
+++ db/query/event-category.query.ts
@@ -1,14 +1,18 @@
 'use server'
 
 import {db} from "@/db/db";
-import {eq} from "drizzle-orm";
+import {desc, eq} from "drizzle-orm";
 import {eventCategory} from "@/db/schema";
 
 export async function getCategoryByEvent(eventId: number) {
   return db.query.eventCategory.findMany({
     where: eq(eventCategory.eventId, eventId),
+    orderBy: desc(eventCategory.name),
     with: {
       participants: {
+        columns: {
+          bibNumber: true,
+        },
         with: {
           user: {
             columns: { name: true }
